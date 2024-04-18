692e32dead17069ba2d13524464be69c950764a1
sran16 <ranimsaidi888@gmail.com>
2024-04-18 23:03:36 +0200

    Fix(update: add itinerary to the update + add comments en

--- routes/v1/planners.js
+++ routes/v1/planners.js
@@ -14,7 +14,8 @@ const prisma = new PrismaClient();
 const MISTRAL_API_KEY = process.env.MISTRAL_API_KEY;
 const prePrompt = process.env.PRE_PROMPT;
 const mistralApiUrl = process.env.MISTRAL_API_URL;
-// Création d'un prompt
+
+// Creating a prompt
 
 router.post("/", async (req, res) => {
   const { prompt } = req.body;
@@ -49,11 +50,11 @@ router.post("/", async (req, res) => {
     res.status(200).json(planner);
   } catch (error) {
     console.log(error);
-    res.status(500).json({error: "An error occurred"});
+    res.status(500).json({ error: "An error occurred" });
   }
 });
 
-// Modification d'un prompt
+// Updating a prompt
 
 router.patch("/:id", async (req, res) => {
   const { id } = req.params;
@@ -63,26 +64,48 @@ router.patch("/:id", async (req, res) => {
     return res.status(400).json({ error: 'The field "prompt" is required.' });
   }
 
-  const planner = await prisma.planners.update({
-    where: {
-      id: String(id),
-    },
-    data: {
-      prompt,
-    },
-  });
+  try {
+    const mistralResponse = await fetch(mistralApiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${MISTRAL_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "mistral-small-latest",
+        messages: [{ role: "user", content: prePrompt + " " + prompt }],
+      }),
+    });
 
-  res.json(planner);
+    const mistralData = await mistralResponse.json();
+    console.log(mistralData.choices[0].message.content);
+
+    const planner = await prisma.planners.update({
+      where: {
+        id: String(id),
+      },
+      data: {
+        prompt,
+        itinerary: JSON.parse(mistralData.choices[0].message.content),
+      },
+    });
+
+    res.json(planner);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "An error occurred" });
+  }
 });
 
-// Récupération des prompts
+// Retrieving prompts
 
 router.get("/", async (req, res) => {
   const entries = await prisma.planners.findMany();
   res.json(entries);
 });
 
-// Récupération d'un prompt
+// Retrieving a prompt by id
 router.get("/:id", async (req, res) => {
   const entry = await prisma.planners.findUnique({
     where: {
